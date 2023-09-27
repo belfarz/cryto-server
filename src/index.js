4965f1e968bfb2515c9a26a5cbd783e0bb4a5f5f
belfarz <115107231+belfarz@users.noreply.github.com>
2023-09-28 00:07:18 +0100

    sgss

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@
  const app = express();
  mongoose.set('strictQuery',false);
 
+ if (process.env.NODE_ENV !== "production" ) {
       require("dotenv").config();
- 
+ } 
 
  const CONNECTION = process.env.CONNECTION;
 
@@ -16,111 +17,6 @@
 
  const PORT = process.env.PORT || 3000;
 
-
-
-
-
-app.get('/', async (req, res)=>{
-   const result = await Customer.findOne({ name: 'john doen' });
-   res.send(result.email);
-});
-
-
-app.get('/api/customers', async (req, res)=>{
-   try {      
-      const result = await Customer.find();
-      res.send({"customers":result});
-   } catch (error) {
-      res.status(500).json({error: error.message});
-   }
-});
-
-app.get("/api/customers/:id", async(req,res)=>{
-   console.log({
-      requestParams:req.params,
-      requestQuery:req.query
-   });
-   try {
-      const {id: customerId} = req.params;
-      console.log(customerId);
-      const client = await Customer.findById(customerId);
-      if (!client) {
-         res.status(404).json({error:"user not found"})
-      }else{
-         res.send({client});
-      }
-   } catch (error) {
-      res.status(500).json({er: "something went wrong"});
-   }
-
-});
-
-app.get("/api/orders/:id",async(req,res)=>{
-   try {
-      const result = await Customer.findOne({"orders._id": req.params.id});
-      if (result) {
-         res.json(result);
-      }else{
-         res.status(404).json({err:"order not found"})
-      }
-   } catch (error) {
-      res.status(500).json({err:"something went wrong"});
-   }
-});
- 
-app.delete("/api/customers/:id", async(req,res)=>{
-   try {
-      const customerId = req.params.id;
-      const result = await Customer.deleteOne({_id:customerId});
-      res.json({deleteCount:result.deletedCount});
-   } catch (error) {
-      res.status(500).json({er: "something went wrong"});
-   }
-})
-
-app.put("/api/customers/:id", async(req , res)=>{
-   try {
-      const {id: customID} = req.params;
-      const result = await Customer.findOneAndReplace({_id: customID},req.body,{new: true});
-      console.log(result); 
-      res.json({result});
-   } catch (error) {
-      res.status(500).json({err:"something went wrong"});
-   }
-});
-
-app.patch("/api/customers/:id", async(req , res)=>{
-   try {
-      const {id: customID} = req.params;
-      const result = await Customer.findOneAndUpdate({_id: customID},req.body,{new: true});
-      console.log(result);
-      res.json({result});
-   } catch (error) {
-      res.status(500).json({err:"something went wrong"});
-   }
-});
-
-app.patch("/api/orders/:id", async(req,res)=>{
-   try {
-      console.log(req.params);
-      const orderId = req.params.id;
-      req.body._id = orderId;  
-      const result = await Customer.findOneAndUpdate( 
-         {"orders._id": orderId},
-         {$set:{"orders.$":req.body}},
-         {new: true}
-      );
-         console.log(result);
-      if (result) {
-         res.json({result});
-      } else {
-         res.status(404).json({erroe:"something went wrong"});
-      }
-   } catch (error) {
-      console.log(error.message);
-      res.status(500).json({error: "something went wrong"})
-   }
-})
 
 app.post('/api/customers', async (req, res)=>{
    console.log(req.body);
