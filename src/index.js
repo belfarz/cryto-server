5c009d94f889a4dee1c2ae2471e497d13a6d535c
belfarz <115107231+belfarz@users.noreply.github.com>
2023-09-28 12:39:07 +0100

    promote pop up

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ app.get("/api/promoted", async (req, res)=>{
    const data = Customer.find()
    try {
       // console.log(undefinedVariable);
-       res.json(data);
+       res.json(data).lean();
    } catch (error) {
       res.status(404).json({error: error.message}) 
    }
