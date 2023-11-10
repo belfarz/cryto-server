6fe9efa78712e55425456858d26fde371290d8f3
belfarz <115107231+belfarz@users.noreply.github.com>
2023-11-10 20:39:17 +0100

    latest endpoint

--- src/index.js
+++ src/index.js
@@ -196,7 +196,7 @@ app.post('/api/coins', async (req, res) => {
    try {
      const { coinIds } = req.body; // Extract coin IDs from the request body
      console.log("hello"+coinIds)
-     const response = await axios.get(`https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?id=${coinIds}`, {
+     const response = await axios.get(`https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?id=${coinIds}&skip_invalid=true`, {
        headers: {
          'X-CMC_PRO_API_KEY': 'f14de90f-4965-4528-8a0f-b41e916cbcf8', 
          "skip_invalid" : true
@@ -206,7 +206,7 @@ app.post('/api/coins', async (req, res) => {
      res.json(response.data);
    } catch (error) {
      console.error(error.message);
-     res.status(500).send('Internal Server Error');
+     res.status(500).send('Internal Server Error'+error.message);
    }
  });
 
